e1736b62b25a1bc95f2138a2df9f938289dba226
dylancorsoo <86756080+dylancorsoo@users.noreply.github.com>
2021-07-14 22:48:50 -0400

    Update http.js

--- http.js
+++ http.js
@@ -1,1 +1,9 @@
 document.write("Corso HTTP Test Zone.")
+
+data = '[{"name" : "Ashwin", "age" : "20"},{"name" : "Abhinandan", "age" : "20"}]';
+
+        var mydata = JSON.parse(data);
+
+        if (mydata[0].name = "Ashwin") {
+            document.write("First name is Ashwin")
+        }
